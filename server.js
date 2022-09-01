f4c5c62ec7b4ce334cddaabe5512cf50732264d4
yayza <y.aezah@gmail.com>
2022-09-01 14:30:33 -0700

    Cleanup

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 3000;
 server.use(requestHandlers.handleAllRequests);
 
 server.get("/geo/:city?/:state?", requestHandlers.handleGeoCodeRequest, async (req, res) => {
-  const city = req.params?.city;
-  const state = req.params?.state;
-
-  const coords = await location.getCoords(city, state);
+  const coords = await location.getCoords(req.params.city, req.params.state);
   res.send(coords);
 });
 
